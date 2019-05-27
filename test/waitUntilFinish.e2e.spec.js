b949d308a5cd6052a46207500f70456a2484ece4
yaroslav-codefresh <yaroslav@codefresh.io>
2019-05-27 22:04:43 +0300

    wait until finish script

---
+++ test/waitUntilFinish.e2e.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const EventEmitter = require('events');
+const Q          = require('q');
+const proxyquire = require('proxyquire').noCallThru();
+const chai       = require('chai');
+const expect     = chai.expect;
+const sinon      = require('sinon');
+const sinonChai  = require('sinon-chai');
+chai.use(sinonChai);
+
+
+
+describe('waitUntilFinish script test', () => {
+
+});

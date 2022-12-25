4ec2e1aa1f7526517a5b3f081fd3779e7e4fbe0b
roi.kramer <roi.kramer@codefresh.io>
2022-12-25 13:16:34 +0200

    Fixed is ready script and replaced with js script

--- lib/isReady.ps1
+++ lib/isReady.ps1
@@ -1,9 +1,25 @@
-CONTAINER_ID=$args[0]
+# DEPRECATED:
+# This script is no longer maintained, you should use the ./isReady.js script
+# Leaving this script for backwards compatibility (so this container logger can work with older engines)
 
-echo "checking if container:$CONTAINER_ID exists"
-if ( !$CONTAINER_ID ) {
-    select-string -Pattern $CONTAINER_ID -Path ./lib/state.json
-}
-else {
-    select-string -Pattern "ready" -Path ./lib/state.json
+$CONTAINER_ID=$args[0]
+
+if ( $CONTAINER_ID ) {
+    echo "checking if container:$CONTAINER_ID exists"
+    if (select-string -Pattern $CONTAINER_ID -Path ./lib/state.json) {
+        echo "container $CONTAINER_ID is ready"
+        Exit 0
+    } else {
+        echo "container $CONTAINER_ID is not ready"
+        Exit 1
+    }
+} else {
+    echo "checking if container logger is ready"
+    if (select-string -Pattern "ready" -Path ./lib/state.json) {
+        echo "ready"
+        Exit 0
+    } else {
+        echo "not ready"
+        Exit 1
+    }
 }

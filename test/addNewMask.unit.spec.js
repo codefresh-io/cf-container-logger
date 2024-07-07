86a6645da2f73c787fc8d20012a45ccfbd58b523
Zhenya Tikhonov <zhenya.tikhonov@codefresh.io>
2024-07-07 11:56:24 +0400

    fix(secrets): fix an issue with secret server listening on IPv6 (#134)
    * fix(secrets): fix an issue with secret server listening on IPv6
    
    * ci: bump version
    
    * feat(secrets): use precise address in `updateMasks()`
    
    * refactor(secrets): replace `request` with `got`
    
    * fix(secrets): store server address on fs
    
    * test(secrets): add tests for helpers
    
    * build: upgrade eslint, clean up dev env
    
    * build: upgrade dependencies
    
    * fix(addNewMask): fail by default
    
    * tests: fix Logger tests

--- test/addNewMask.unit.spec.js
+++ test/addNewMask.unit.spec.js
@@ -1,78 +1,181 @@
-/* jshint ignore:start */
-
-'use strict';
-const Q = require('q');
-
-const chai       = require('chai');
-const expect     = chai.expect;
-const sinon      = require('sinon');
-const sinonChai  = require('sinon-chai');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const proxyquire = require('proxyquire').noCallThru();
+
+const expect = chai.expect;
 chai.use(sinonChai);
 
-const originalProcessExit = process.exit;
 
 describe('addNewMask', () => {
-    before(() => {
-        process.exit = sinon.spy();
+    const originalProcessExit = process.exit;
+    const originalConsole = console;
+
+    const stubGetServerAddress = sinon.stub();
+    const stubProcessExit = sinon.stub();
+    const stubConsole = {
+        debug: sinon.stub(),
+        error: sinon.stub(),
+        warn: sinon.stub(),
+        log: sinon.stub(),
+    };
+    const stubGot = {
+        post: sinon.stub().resolves({ statusCode: 201 }),
+    };
+
+    const secret = {
+        key: '123',
+        value: 'ABC',
+    };
+
+    before(async () => {
+        process.exit = stubProcessExit;
+        console = stubConsole;
+        const { default: httpClient } = await import('got');
+        sinon.stub(httpClient, 'post').callsFake(stubGot.post);
     });
 
     beforeEach(() => {
-        process.exit.resetHistory();
+        stubProcessExit.resetHistory();
+        stubGetServerAddress.resetHistory();
+        for (const stub in stubConsole) {
+            stubConsole[stub].resetHistory();
+        }
+        for (const stub in stubGot) {
+            stubGot[stub].resetHistory();
+        }
     });
 
     after(() => {
         process.exit = originalProcessExit;
+        console = originalConsole;
     });
 
     describe('positive', () => {
         it('should send a request to add a secret', async () => {
-            const rpSpy = sinon.spy(() => Q.resolve({ statusCode: 201 }));
-            const addNewMask = proxyquire('../lib/addNewMask', {
-                'request-promise': rpSpy
-            });
+            const serverAddress = 'https://xkcd.com/605/'
+            stubGetServerAddress.resolves(serverAddress);
+            stubGot.post.resolves({ statusCode: 201 });
 
-            process.env.PORT = 1337;
-            process.env.HOST = '127.0.0.1';
-    
-            const secret = {
-                key: '123',
-                value: 'ABC',
-            };
-
-            addNewMask(secret);
-    
-            expect(rpSpy).to.have.been.calledOnceWith({
-                uri: `http://127.0.0.1:1337/secrets`,
-                method: 'POST',
-                json: true,
-                body: secret,
-                resolveWithFullResponse: true,
+            const { updateMasks, exitHandler } = proxyquire('../lib/addNewMask', {
+                './helpers': { getServerAddress: stubGetServerAddress },
             });
-            await Q.delay(10);
-            expect(process.exit).to.have.been.calledOnceWith(0);
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            await updateMasks(secret);
+            expect(stubGot.post).to.have.been.calledOnceWith(new URL('secrets', serverAddress), {
+                json: secret,
+                throwHttpErrors: false,
+            });
+            expect(stubProcessExit).to.have.been.calledOnceWith(0);
         });
     });
 
     describe('negative', () => {
-        it('should send a request to add a secret', async () => {
-            const rpSpy = sinon.spy(() => Q.reject('could not send request'));
-            const addNewMask = proxyquire('../lib/addNewMask', {
-                'request-promise': rpSpy
+        it('should fail if the server address is not available', async () => {
+            stubGetServerAddress.rejects('could not get server address');
+            const { updateMasks, exitHandler } = proxyquire('../lib/addNewMask', {
+                './helpers': {
+                    getServerAddress: stubGetServerAddress,
+                },
             });
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            await updateMasks(secret);
+            expect(stubConsole.error).to.have.been.calledOnceWith('could not create mask for secret: 123. Error: could not get server address');
+            expect(stubProcessExit).to.have.been.calledOnceWith(1);
+        });
 
-            process.env.PORT = 1337;
-            process.env.HOST = '127.0.0.1';
-    
-            const secret = {
-                key: '123',
-                value: 'ABC',
-            };
+        it('should fail if the server address is not valid URL', async () => {
+            stubGetServerAddress.resolves('foo');
+            const { updateMasks, exitHandler } = proxyquire('../lib/addNewMask', {
+                './helpers': {
+                    getServerAddress: stubGetServerAddress,
+                },
+            });
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            await updateMasks(secret);
+            expect(stubConsole.error).to.have.been.calledOnceWith('could not create mask for secret: 123. Error: TypeError: Invalid URL');
+            expect(stubProcessExit).to.have.been.calledOnceWith(1);
+        });
 
-            addNewMask(secret);
-            await Q.delay(10);
-            expect(process.exit).to.have.been.calledOnceWith(1);
+        it('should fail if server responded not with 201', async () => {
+            const serverAddress = 'https://g.codefresh.io'
+            stubGetServerAddress.resolves(serverAddress);
+            stubGot.post.resolves({
+                statusCode: 500,
+                body: 'Internal Server Error',
+            });
+            const { updateMasks, exitHandler } = proxyquire('../lib/addNewMask', {
+                './helpers': { getServerAddress: stubGetServerAddress },
+            });
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            await updateMasks(secret);
+            expect(stubConsole.error).to.have.been.calledOnceWith('could not create mask for secret: 123. Server responded with: 500\n\nInternal Server Error');
+            expect(stubProcessExit).to.have.been.calledOnceWith(1);
+        });
+    });
+
+    describe('exitHandler', () => {
+        it('should set exit code to 3 if the original exit code is 0 and variable was not masked', () => {
+            const { exitHandler } = proxyquire('../lib/addNewMask', {});
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            exitHandler(0);
+            expect(process.exitCode).to.be.equal(3);
+            expect(stubConsole.warn).to.have.been.calledOnceWith('Unexpected exit with code 0. Exiting with 3 instead');
+            process.exitCode = undefined;
+        });
+
+        it('should set exit code to 3 if the original exit code is 0 and variable was not masked', () => {
+            const { exitHandler } = proxyquire('../lib/addNewMask', {});
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            if (process.exitCode !== undefined) {
+                throw new Error('process.exitCode should be undefined to run this test');
+            }
+            process.exitCode = 0;
+            exitHandler();
+            expect(process.exitCode).to.be.equal(3);
+            expect(stubConsole.warn).to.have.been.calledOnceWith('Unexpected exit with code 0. Exiting with 3 instead');
+            process.exitCode = 0;
+        });
+
+        it('should not change exit code if the variable was masked successfully', async () => {
+            const serverAddress = 'https://xkcd.com/605/'
+            stubGetServerAddress.resolves(serverAddress);
+            stubGot.post.resolves({ statusCode: 201 });
+            const { updateMasks, exitHandler } = proxyquire('../lib/addNewMask', {
+                './helpers': { getServerAddress: stubGetServerAddress },
+            });
+            process.listeners('exit').forEach((listener) => {
+                if (listener === exitHandler) {
+                    process.removeListener('exit', listener);
+                }
+            });
+            await updateMasks(secret);
+            expect(process.exitCode).not.to.be.equal(3);
+            expect(stubConsole.warn).not.to.have.been.calledOnceWith('Unexpected exit with code 0. Exiting with 3 instead');
         });
     });
 });
-
